7d82d4f5e60f36e9ce11a010153e0e904d29e7c2
Anderson Jhones <98041913+andersonjhones@users.noreply.github.com>
2023-08-10 15:59:32 -0300

    Add files via upload

---
+++ CrudComJavaScript/objetos.js
@@ -0,0 +1,26 @@
+const produtos = [
+    {
+        nome: 'Teclado',
+        valor: 250,
+        estoque: 10
+    },
+    {
+        nome: 'Mouse',
+        valor: 150,
+        estoque: 20
+    }
+];
+const clientes = [
+    {
+        nome: 'Kelwin',
+        saldo: 1500,
+        produtos: []
+    },
+    {
+        nome: 'Karla',
+        saldo: 400,
+        produtos: []
+    }
+];
+
+module.exports = {produtos:produtos, clientes:clientes};
